7acc96bb4b564aa95a0c85ee5118bc3b11839e37
Natthew Denson <mdenson@dayspring-tech.com>
2016-11-12 02:03:25 +0000

    Really adding some aliases this time.

---
+++ zsh.custom/symfony.zsh
@@ -0,0 +1,6 @@
+# aliases for working with symfony
+
+alias sf="app/console"
+alias pmb="app/console propel:model:build"
+alias mig="app/console propel:migration:migrate"
+alias migdiff="app/console propel:migration:generate-diff"
